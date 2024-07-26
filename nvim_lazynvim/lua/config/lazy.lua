4247bf49e34ef7519ff6fd025eb7f3db56e827e0
aleksandrov_ds <aleksandrov_ds@smart-transport.ru>
2024-07-26 12:22:55 +0500

    Update

--- nvim_lazynvim/lua/config/lazy.lua
+++ nvim_lazynvim/lua/config/lazy.lua
@@ -17,9 +17,13 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
   spec = {
     -- add LazyVim and import its plugins
-    { "LazyVim/LazyVim", import = "lazyvim.plugins", opts = {
-      colorscheme = "solarized-osaka",
-    } },
+    {
+      "LazyVim/LazyVim",
+      import = "lazyvim.plugins",
+      opts = {
+        colorscheme = "solarized-osaka",
+      },
+    },
     -- import/override with your plugins
     { import = "plugins" },
   },
@@ -52,6 +56,4 @@ require("lazy").setup({
       },
     },
   },
-
-  { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} },
 })
--- nvim_lazynvim/lua/plugins/colorscheme.lua
+++ nvim_lazynvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,20 @@
 return {
-  "craftzdog/solarized-osaka.nvim",
-  lazy = true,
-  priority = 1000,
-  opts = {
-    transparent = true,
+  {
+    "craftzdog/solarized-osaka.nvim",
+    lazy = true,
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
   },
 }
