fe4788268bdb80b6a350c3089689f7bb0c3ad833
Dmitriy Aleksandrov <Adsis369@gmail.com>
2025-02-13 15:58:32 +0500

    try release script for qmk-hid-host

--- scripts/qmk_hid_host_zellij.sh
+++ scripts/qmk_hid_host_zellij.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-# my_script.sh
-echo "This is custom script running qmk-tools in Zellij session."
-cd /Users/ads/dotfiles/vial/qmk-hid-host
-./qmk-hid-host
+
+SESSION_NAME="qmk-hid-host"
+
+if zellij list-sessions | grep -q "^$SESSION_NAME$"; then
+  echo "Session $SESSION_NAME already exists. Detaching..."
+  zellij attach --session "$SESSION_NAME" --detach
+else
+  echo "Creating session $SESSION_NAME and starting the program..."
+  zellij --session "$SESSION_NAME" run -- ./Users/ads/dotfiles/vial/qmk-hid-host/qmk-hid-host
+fi
